516c54e7a4cbb78e0446b2197a792701eddb146b
gabgren <github@gabgren.com>
2023-09-28 22:15:09 -0400

    ui improvements and threading

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 import subprocess
+import threading
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -10,6 +11,7 @@ from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.metrics import sp, dp
+from kivy.clock import Clock
 
 label = None
 log = None
@@ -25,7 +27,7 @@ def zip_directory(folder_path, output_filename):
             for filename in filenames:
                 file_path = os.path.join(foldername, filename)
                 zipf.write(file_path)
-                
+                  
 class DropTarget(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -33,17 +35,25 @@ class DropTarget(Widget):
         self.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
         Window.bind(on_drop_file=self._on_drop_file)
 
+    def update_label(self, new_text):
+        global label
+        label.text = new_text
+
+    def update_log(self, new_text):
+        global log
+        log.text = new_text
+        log.opacity = 1
+        
     def _on_drop_file(self, window, file_path, x, y):
         file_path = file_path.decode('utf-8')
         print("File dropped: " + file_path)
-        if os.path.exists(file_path):
-            self.zip_file(file_path)
+        threading.Thread(target=self._zip_file, args=(file_path,)).start()
 
-    def zip_file(self, file_path):
+    def _zip_file(self, file_path):
         global label
         with open(keychain_profile_save, "w") as f:
             f.write(keychain_profile.text)
-        label.text = "Zipping..."
+        Clock.schedule_once(lambda dt: self.update_label("Zipping..."))
         zipped_path = file_path + ".zip"
         # remove zip if it already exists
         if os.path.exists(zipped_path):
@@ -51,11 +61,11 @@ class DropTarget(Widget):
         # zip the app
         zip_directory(file_path, zipped_path)
         print(f"Zipped {file_path} to {zipped_path}")
-        self.notarize_file(zipped_path)
+        self._notarize_file(zipped_path)
     
-    def notarize_file(self, file_path):
+    def _notarize_file(self, file_path):
         global label, log
-        label.text = "Notarizing..."
+        Clock.schedule_once(lambda dt: self.update_label("Notarizing..."))
         command = f"xcrun notarytool submit --keychain-profile {keychain_profile.text} --wait \"{file_path}\""
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, text=True)
 
@@ -74,28 +84,25 @@ class DropTarget(Widget):
         return_code = process.wait()
 
         if return_code != 0:
-            label.text = "Notarization failed"
-            log.text = result
-            log.opacity = 1
+            Clock.schedule_once(lambda dt: self.update_label("Notarization failed"))
+            Clock.schedule_once(lambda dt: self.update_log(result))
         elif not valid:
             command = f"xcrun notarytool log {submission_id} --keychain-profile {keychain_profile.text}"
             result = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True, text=True).strip()
-            label.text = "Notarization failed"
-            log.text = result
-            log.opacity = 1
+            Clock.schedule_once(lambda dt: self.update_label("Notarization failed"))
+            Clock.schedule_once(lambda dt: self.update_log(result))
         else:
             print(f"Notarization was successful! Submission ID: {submission_id}")
-            self.staple_file(file_path)
+            self._staple_file(file_path)
 
-    def staple_file(self, file_path):
+    def _staple_file(self, file_path):
         global label, log
-        label.text = "Stapling..."
+        Clock.schedule_once(lambda dt: self.update_label("Stapling:"))
         file_path = file_path.replace(".zip", "")
         command = f"xcrun stapler staple \"{file_path}\""
         result = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True, text=True).strip()
-        label.text = "Result:"
-        log.text = result
-        log.opacity = 1
+        Clock.schedule_once(lambda dt: self.update_label("Result:"))
+        Clock.schedule_once(lambda dt: self.update_log(result))
         
 
 
@@ -103,16 +110,12 @@ class MyApp(App):
     def build(self):
         global label, keychain_profile, log
         
+        self.title = 'Notarizer'
+        
         layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
 
-        label = Label(text="Drag and drop a file here to zip it!")
-        layout.add_widget(label)
-                
-        layout.add_widget(Widget(size_hint=(1, None), height=40))
-        
         hbox = BoxLayout(orientation='horizontal', spacing=10, padding=10, size_hint_y=None, height=dp(40))
         hbox.add_widget(Label(text="Keychain profile:", size_hint=(None, 1), width=250))
-        
         # look in user's cache dir if they have a keychain profile saved
         # if they do, use that as the default value
         keychain_profile_name = "default"
@@ -121,8 +124,13 @@ class MyApp(App):
                 keychain_profile_name = f.read()
         keychain_profile = TextInput(text=keychain_profile_name)
         hbox.add_widget(keychain_profile)
-        
         layout.add_widget(hbox)
+
+        label = Label(text="Drag and drop a .app, .bundle or .plugin here to notarize it.")
+        layout.add_widget(label)
+                
+        layout.add_widget(Widget(size_hint=(1, None), height=40))
+        
         
         log = TextInput(text="", readonly=True, opacity=0)
         layout.add_widget(log)
